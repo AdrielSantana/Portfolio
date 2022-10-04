635829983bf2d6a1d896ff17ee9a3ea8c726c85f
Adriel Santana <diel_san@hotmail.com>
2022-10-04 12:42:56 -0300

    adição da page _document e mudança no hero

--- pagina_portfolio_next/src/components/layout/HeroTitle.tsx
+++ pagina_portfolio_next/src/components/layout/HeroTitle.tsx
@@ -1,31 +1,19 @@
-import { useTypewriter } from 'react-simple-typewriter'
+import { Typewriter } from 'react-simple-typewriter'
 import { useState } from 'react'
 
 const HeroTitle = () => {
-    const [restWord, setRestWord] = useState<number | string>('')
-
-    const [typeError] = useTypewriter({
-        words: ['','ho', 'io :)'],
-        loop: 1,
-        delaySpeed: 300,
-        deleteSpeed: 90,
-        typeSpeed: 90
-    })
-
-    const [welcome] = useTypewriter({
-        words: ['Bem vindo ao meu Portfol'],
-        loop: 1,
-        delaySpeed: 100,
-        deleteSpeed: 90,
-        typeSpeed: 90,
-        onLoopDone: () => setRestWord(typeError)
-    })
-
     return (
         <>
             <div className='title'>
-                <p>
-                    {welcome}{restWord}
+                <p className='text-center'>
+                    <Typewriter
+                    words={["Bem vindo ao meu Portfolio :)","Desenvolvedor Web Full-Stack","Dá uma olhadinha nos projetos ;)", "Não esquece de mandar mensagem!", "Cansei de escrever :P", "Tá esperando oque pra ir ver?", "Bem vindo ao meu Portfolio :)"]}
+                    typeSpeed={90}
+                    deleteSpeed={50}
+                    delaySpeed={5000}
+                    loop={1}
+                    cursor={true}
+                    />
                 </p>
             </div>
         </>
