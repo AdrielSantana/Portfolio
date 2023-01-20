83dfcfaa5496e491665ee91e70d971731dab772e
Adriel Santana <diel_san@hotmail.com>
2023-01-20 14:42:10 -0300

    adicionando analytics

--- pagina_portfolio_next/pages/_app.tsx
+++ pagina_portfolio_next/pages/_app.tsx
@@ -1,10 +1,17 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../src/styles/main.scss'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../src/styles/main.scss";
 
-import type {AppProps} from 'next/app'
+import { Analytics } from "@vercel/analytics/react";
 
-function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
