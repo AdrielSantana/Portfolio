bdd22a621f3125fff044e214bd24beae7cee2324
Adriel Santana <diel_san@hotmail.com>
2023-02-22 17:01:53 -0300

    Projeto Game Awards adicionado e mudança nas descriptions dos projetos

--- pagina_portfolio_next/src/types/projects.ts
+++ pagina_portfolio_next/src/types/projects.ts
@@ -5,6 +5,7 @@ import meuClimaImg from "../../public/images/projects/meuClima.png";
 import spaceXImg from "../../public/images/projects/spaceX.png";
 import portfolioImg from "../../public/images/projects/portfolio.png";
 import githubImg from "../../public/images/projects/github.png";
+import gameAwards from "../../public/images/projects/gameAwards.png";
 
 import { StaticImageData } from "next/image";
 
@@ -19,42 +20,49 @@ export const projects: Project[] = [
   {
     name: "Tomenote",
     description:
-      "Projeto Full-Stack que se baseia no conceito do App Evernote ( criação e edição de notas ). Utilizei Figma, Sass, BootStrap, ReactJs, NodeJS e MongoDB.",
+      "Projeto Full-Stack Web. Crie e edite notas de onde quiser. Utilizei Figma, Sass, BootStrap, ReactJs, NodeJS e MongoDB.",
     imgUrl: tomenoteImg,
     link: "http://www.tomenote.adrielsan.social",
   },
   {
+    name: "Game Awards",
+    description:
+      "Projeto Full-Stack Mobile. Vote nos jogos e categorias do Game Awards. Utilizei Figma, Sequelize, Express, Postgres e React Native / Expo.",
+    imgUrl: gameAwards,
+    link: "https://github.com/AdrielSantana/game-awards",
+  },
+  {
     name: "OneBitLife",
     description:
-      "Projeto mobile completo mostrando as principais funcionalidades do React Navite e Expo. Utilizei React Native e SQLite.",
+      "Projeto Full-Stack Mobile. Cumpra bons hábitos para ajudar o robô. Utilizei React Native / Expo e SQLite.",
     imgUrl: oneBitLifeImg,
     link: "https://github.com/AdrielSantana/Projeto-React-Native-OBC",
   },
   {
     name: "Catálogo Apple",
     description:
-      "Projeto utilizando o framework Next Js e TypeScript simulando um catálogo com carrinho de compras.",
+      "Projeto Front-End Web. Simula um catálogo com carrinho de compras. Utilizei Next Js e TypeScript.",
     imgUrl: catalogoAppleImg,
     link: "http://www.catalogoapple.adrielsan.social",
   },
   {
     name: "Meu Clima",
     description:
-      "Projeto Front-End React projetado para mostrar o clima local, atual e previsões consumindo uma API externa.",
+      "Projeto Front-End Web. Mostrar o clima local, atual e previsões consumindo uma API externa. Utilizei React.",
     imgUrl: meuClimaImg,
     link: "http://www.climalocal.adrielsan.social",
   },
   {
     name: "SpaceX",
     description:
-      "Projeto Front-End inspirado no site da SpaceX, mostrando sua funcionalidade e responsividade Front-End, utilizando apenas HTML, CSS, Bootstrap e Sass.",
+      "Projeto Front-End Web. Insparado no site da SpaceX, mostrando sua funcionalidade e responsividade Front-End. Utilizei apenas HTML, CSS, Bootstrap e Sass.",
     imgUrl: spaceXImg,
     link: "http://www.spacex.adrielsan.social",
   },
   {
     name: "Meu Portfolio",
     description:
-      "Projeto utilizando o framework Next Js e TypeScript para apresentar meu portfolio com um visual limpo, moderno, responsivo e de alta performance.",
+      "Projeto Front-End Web. Apresentar meu portfolio com um visual limpo, moderno, responsivo e de alta performance. Utilizei Next Js e TypeScript.",
     imgUrl: portfolioImg,
     link: "",
   },
