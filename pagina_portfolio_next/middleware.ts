b14e9182af661dfc730fc967c3592dc4029dac0c
Adriel Santana <diel_san@hotmail.com>
2023-05-22 22:59:52 -0300

    og consertado

--- pagina_portfolio_next/middleware.ts
+++ pagina_portfolio_next/middleware.ts
@@ -43,5 +43,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|favicon).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|favicon|opengraph-image.png).*)"],
 };
