c3d08c9fd9f69dd889d83f692938c6a242b62a47
Adriel Santana <diel_san@hotmail.com>
2023-05-24 19:18:35 -0300

    resolvi email

--- pagina_portfolio_next/middleware.ts
+++ pagina_portfolio_next/middleware.ts
@@ -20,6 +20,7 @@ function getLocale(request: NextRequest): string | undefined {
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+  console.log(pathname)
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one of these directories as a public directory, remove this if condition.
